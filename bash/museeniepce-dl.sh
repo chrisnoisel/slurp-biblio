60b213433d09ff96a47e8c7dd69d281113162cf2
Christophe Noisel <chrisnoisel@users.noreply.github.com>
2019-03-18 00:28:35 +0100

    added script for removing watermark from open-museeniepce.com

---
+++ bash/museeniepce-dl.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+# http://www.open-museeniepce.com
+
+watermark='iVBORw0KGgoAAAANSUhEUgAAAcAAAABJCAAAAACPhqkLAAANOUlEQVR42u2a/WvbWLrHPz1zRldo
+NVrVFVojjPE1oQQTShlKKGVYlrI/3D/7Ukpvb2/IDSFkQzAmeLwer8fXo1U1Wo1Gq7k/6NUvcpwm
+250FfaElUY6el/M853k7esRnxrNuLL0396MhZCLi5JZFyis9EaNLfjXYlLr9Ipa8W96LqvzMWuhd
+JPvuarcXoM6GAFiHoeJmLx51Y3mraRwdEVztXDIwI/Vy+dAqOgcB6iLlbAwS6Z+nzw/7sRyuCNRD
+Mron/89twC5wsdhzsWkYoI8jANUE9TIXWiJue7kDfNh9TNsaaA9uQMMwwJz4AIoFan4C16U2LFjc
+N0SIzxxGHJje7Ls6BpDt1d+BZI93LRPO/T3oPziSwn9ud+bkjH8tA/ZVwvM7vtP5JKEPYDL+J2Y8
+59YVWg/OwvvyqYRQCxvAjbzoH6aWd5nM7+r3rdZ6lLu6SURwi2NOZ/Hsn1myaO119sPpmtSXcbDg
+oQyomFaeeLCD+fbtkUmy9sNtp3t92Xy+bZ3YTc5ZN2AQbHBaJ5DUnL6tkm++XiOSSPaXurNuwHDt
+tAWjml1L9tmgfFlmQMtePdzetPpGX0viazloy8S7dFEPbZn417kplD4iGiWAY0XKZFk8tlVB5A69
+SmX/1FZIgulNhbg8aKsQzEb1B9O5Wvtb246UWeEK6oGtkPgrAbPVNyWxfzOrvCKH4XNbJu6Vu5qx
++j2VcDysiNRva+CXYoq+kohhNOjKq3xZp2tAtBx5OyqkNTez21Wp6XZ1iBbDoGRCNGwNdBFNrysK
+b1PPPDAliXczyw3Y04DA9yLAUkww1HEljvY0An+ggmh98yZ4LUCYxxdZLaIcQDKOAKcN/jJvcdK9
+0JyLomaxj7PD3n9TEDe+EWnl1n+7veIIFaE4a8fJcSDJt8L5Ogu03XeFXwz6AFK1pqeVV0K7BcL6
+5rxKTjzrAtph60Mh0isJYJoH7zKRxIGEWb9d+Lt42Uq161xsr8hiIXCGawbsVqSWrwwA2e2eTksm
+vngKqH3nbXFat6l3eJCeOmt2ktYDPQ2C0c0iAlhMrxagdJUV7uqzrBr++pushDisLNg44caLoso7
+MvNnx/lK9UVB9pWAJAbk8fbSZLbcLGOq3KyvC7c8LgqYfs7JOay88rSVzRLMai/YzbbXKUQq0spL
+Ub4dH5XVMMetQjtru9Tz7XVoIfmxkVP42ij/ph1kMhRKtUr1ik3rHeTqtQdIwNZgXkmnydzrKEpn
+tBqGw1F8oIFOPAr6Bkhrurvd804C7aUG3SxkHQKT86R3BC0ne/dQwvw0br8ArbvVmX3PomXUR6oB
+EFz4/R5YGVX1KXA+5nkH+qOokjZGrtMGDk4qYT0eBh0bsCsixRdLeyBQD8suTRqQ93SOBeGJqx3r
+0N9ah4RuG92e1+9PC4ITTz/WoH9WkdAfcaBBKy+BMvV6fbCzZ/IQuBxx1IPeUIBqwXQBaPZgMOib
+QDiOUFd9K3o7Gqd6v7uefEgAfVflCFwEBJdA5mGGDd5Zws0NkHmz6kB4EjO7Kp9tlJM7myrLAD7M
+g4tZSaErYDiGMx9ENbmfXk5PxkBbrXjr2+H0w7JUR3GA00lwcw50qlEhPHtzOinc88wlOAUsdesc
+b5bs7CS6wKmHfwbYlU4gfDsev41KjVtmqt5lVT0JoxFceCBsAW1YuIDdswDVcYBoAtZKTLsJwfOB
+iQfhbeMLCYSAB2Sh1gamADMgC2KWgFmSPTO2xlA1Xqxt5KoBgakPzEtXsXJOlWcA/hQY5f6VYeRn
+7ClF8ubAJASlsjB6M/GmC0g93lsAngfC2GpAZuCotT2GCa4LLH1QKhTGMUTjcoNsYJarZ66pl22a
+phHNAbvoIywgXCBaVZ5untSW+8ww4ox5eHF1fZ2dwJyIDygya1myZ1ECirq9qp6A0q6dW1WpqjIP
+dqGXP6vEiQWAH6xZFSAq01NBEHd1YRmLzcwz05e1WqlFp25mtEqhovcy/081CmmWxaYJAGFC5AEE
+gCHRU80UC4JZiGNiLRNwLczFevoVe86x8Aw4Mod+UuY1HZBCJdIBqcR5SpFoRBqAsr37nMWSzrSG
+rZYpgvcur6VUCYmKIJCrk4q0svO1lS1br4r0YmGwah13hSdoEMr6afI80HCGO6QuKShr7pROUck3
+qFQvfaQAaiwJFEBIdBIvdYroBpgKQ7QWEHmGooafOnybdIBOx59MwmpUfbFFkaOj207zrIPN9umQ
+UPJwEBdRXQW0P26rLIrgoOwO/nGxUG4zsgp0OrddB0wPMNSaSZkKdLtbVE1VSUTmY9vUUwDldaW6
+VAmzksTNA42Rqat+8oAnG7Lrh388zjZLyE+exI4BZ/8BnNg9ZU5uv4OJty0UNQxqDUh97t6td6Up
+EzuHnwBSphkANTu6hFnpHN3rrmm0zPo/+/WbAEAKIIhTiWRcbmYYicyV6my09Ayc2Z0MmOTRWAs+
++T5B1O10AsSBuIWBt7BwdlyHRGFO4W6jZ1FEWUALCgNuZLd73rW4b7VORwPks/dpXABO3U1hzue3
+0poaayVxfV+f/Rr8Z63f73HqZWmnui2cXNyeRiwMJ6qN1uOrO4eQQqzoTaGzjMuMsF9o2BvB9bX+
+3ARLC4AkVrbHqj34TA+Rdn20kQCiK9MCnKRuD5RCy2h3+JRr2XBrhL0Vs1jSFrVMtv1FyjjPNjFA
+EimZNLIrRDjewr0wYCLQQwC1TzhK1Y0/JfIAqEdCmdyA//61AkYAEGkZJ/lKI3wbAwRmrsjLVpy8
+q4tGwbS2Jw7UjKp6BME4rx6lSADrOFLH52sG1HYbMCgWykqo2iiGlGwoGWhnkzpXmHbrDlNUUBj0
+A+2kTA9KmLcVqYihnqs3yNUL0+sJoPUyUsciCVFF2paYeZ/oZUkxvFtgLmsepW2b9mrxhJeXz4oh
+U0/LOhkApSUUpd5f6od2ft7gyqLMDCNQNABDqNUtNAEUvejBtqeuoso36gxYtpc62o6AN9mLibbe
+IGIAiV+wMvINTv07DEBqALpQkQI/HSa4oPRUhGOQLAHFIAr3PXdSB/Sy8Q+T3JxFHYyb9fbY5Q4u
+8qlIS4Bffy5mtZIsqlTTDJssC6or7RuWDmlQc+vVWQK2AmgtiLcudGMwdEBaeZ+9nVQtGy+Blgqo
+ayI6+X9uVKjXFqubtsymMViAK/DTn4IFaP3BoQmLJP2ru1+qC4GBLuyXlRDhg9EBnBZEqYrzBOxO
+NmrOCpdFAGYvm89OdxUEtaaNQHsO5tN81JKOmA7V9HqLaon0whDtAeDt0CyYgXiW/mO+1afieTZl
+PpTg7ig0azUKCwoCFhXvtJ4pcmCVw715COqzTL15qd5TLbs/mUmCQFPsOczJZmnugvQjMG+/8Ll0
+wPzD2r4a8LwTGCYwTnNjeNOH552wLcHPBzQ3Azhy/LaSjRFqDXhQd/6HA+jYkQ6483yxjvKHmWID
+N9W8qv8+47pLnes2tP8jVCVwXdMjOWC/XphG/ZJ0Fwa1XVYbHGPZ0tcpZO19lF1ZJqNSvWXun66J
+/P1cpuoJmKefcDG/WQDBZAoovXX3qy9R82snt+LsQx+wuiYwz8vlyyVgdSTExeXpaAa0ugrwId6a
+WVO+/qIqQ1WW0SRPbOUXXqeA7NjAolrtu5kxZ+N1LUTlF+8MkLoETv3thb17CWhdA7ie7+gCglnd
+35ZXgN7VgYtKDI7y4eV5vKleUY2dxiAdG1he8AX8/Ejjq18C+PnH77//3v0J0HqC8M8F3Sfib3+f
+hMBjxec7HzB+81F+99f87ut3Apj/1y9PPorpRwB+GX9pPAII/lReqX0rzEcAk/c/lVEmfvIIYPZ+
+SyjSf/tRzH5IXfHJxxD3u/T5V199lNlzmIWPvwBY/PeP+Xs/jQ0NIPpTYb+2AaPzjgSGxU48/tIX
+f/4RUJ/4/N9fig78t/8G4J58X2wAf/v7t6XQ/OA+/hLA+99vtww6H38Ui/TVyPLDxJuW2vzlhyI/
+PpEA7ul3uW3//Qu+eOe1geh/5pX0n6o3f1/Ekp8n+m8A4qtzeATZjXxZKIi2CdF4/wGBMLXYXa8z
+hKGK2A/WnmmEXryxLnLvNTYwNLH+KZ1iKPiVHPC8AzcXmHq83EMvzRCxt7uE0/UN7e4IXV9hIl8r
+8MaTLSVY3kG9RwB0DFa/ibmT/f4VkBnwV4zMgHd+LU37pgOaVk47Vr9KE5/n89+NU/gQfLd8kPew
+2iTJfelvirj/FCzJlrqeVfmCYv270FfmZziOQm6439ODe/NVZifrjx5WG+V6vQ7tH0Z30/t8/RvW
+1qs42ZP7PLd1Mp9nX2Z74caX2cquC7QHNCH/AL6bFNSH1UZuPlA+iUL1vmrvIyhldaixqI1t8Wcw
+n0g2R4b35pvN7EiIZUY/Uh9SG5ncd7dyESOZiFzYJLnbuw0aNGjQoEGDBg0aNGjQoEGDBg0aNGjQ
+oEGDBg0aNGjQoEGDBg0aNGjQoEGDBg0aNGjQoEGDBg0aNGjQoEGDBg0aNGjQoEGDBg0aNGjQ4NeK
+/we8RwUIxgrnZAAAAABJRU5ErkJggg=='
+
+function usage
+{
+	echo -e "usage :" >&2
+	echo -e "\t"$(basename "$0")" <page_url>" >&2
+	echo -e "\t"$(basename "$0")" <watermarked_image_url>" >&2
+	echo -e "\t"$(basename "$0")" <watermark_image_file>" >&2
+}
+
+function revert_watermark
+{
+	{
+		echo "$watermark" | base64 -d | convert "$1" -gravity South png:- -compose Minus_Src -composite miff:-
+		echo "$watermark" | base64 -d | convert png:- -channel RGB -negate miff:-
+	} | convert miff:- -gravity South -compose Divide_Src -composite png:"$2"
+}
+
+if [ -z "$1" ]
+then
+	echo "error: missing argument"
+	usage
+	exit 1
+fi
+
+watermarked=""
+tmp_file=""
+
+if [ -n "$(echo $1 | grep http)" ]
+then
+	# url
+	urlimg=""
+	if [ -z "$(echo $1 | grep 'protectimage')" ]
+	then
+		urlimg=$(curl "$1" | grep og:image | grep -Eo 'http[^"]+' | sed -E 's/(&|\?)w=[0-9]+//')
+	else
+		urlimg="$1"
+	fi
+	
+	tmp_file=$(mktemp)
+	watermarked="$tmp_file"
+	curl -o "$tmp_file" "$urlimg"
+
+elif [ -e "$1" ]
+then
+	watermarked="$1"
+fi
+
+revert_watermark "$watermarked" /dev/stdout
+
+if [ -n "$tmp_file" ]
+then
+	rm "$tmp_file"
+fi
